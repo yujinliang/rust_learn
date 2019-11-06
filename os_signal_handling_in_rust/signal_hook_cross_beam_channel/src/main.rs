6329e4872a15cc22929d5ef1c6b68d9808e9de3f
yujinliang <285779289@qq.com>
2019-11-06 18:42:11 +0800

    signal_hook&&cross_beam_channel

--- os_signal_handling_in_rust/signal_hook_cross_beam_channel/src/main.rs
+++ os_signal_handling_in_rust/signal_hook_cross_beam_channel/src/main.rs
@@ -1,3 +1,25 @@
+extern crate crossbeam_channel as channel;
+extern crate signal_hook;
+
+use std::{thread,os::raw::c_int, error::Error};
+
+fn notify(signals: &[c_int]) -> Result<channel::Receiver<c_int>, Box<dyn Error> > {
+
+    let (s, r) = channel::bounded(100);
+    let signals = signal_hook::iterator::Signals::new(signals)?;
+    thread::spawn(move || {
+
+        for signal in signals.forever() {
+            s.send(signal).unwrap();
+        }
+    });
+    Ok(r)
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let r = notify(&[signal_hook::SIGINT]).unwrap();
+
+    println!("catch the signal : {:?}", r.recv().unwrap());
+
 }
