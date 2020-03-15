bc3189d56828935e12f9eb1a13d718a4894ee205
yujinliang <285779289@qq.com>
2020-03-15 20:38:10 +0800

    remove garbage code

--- tokio/select_macro/target/rls/debug/deps/select_macro-716c196d91609a14.d
+++
@@ -1,5 +0,0 @@
-/home/yjl/study_proj/rust_learn/tokio/select_macro/target/rls/debug/deps/select_macro-716c196d91609a14.rmeta: src/main.rs
-
-/home/yjl/study_proj/rust_learn/tokio/select_macro/target/rls/debug/deps/select_macro-716c196d91609a14.d: src/main.rs
-
-src/main.rs:
--- tokio/select_macro/target/rls/debug/deps/tokio_macros-48385d003f70045d.d
+++
@@ -1,7 +0,0 @@
-/home/yjl/study_proj/rust_learn/tokio/select_macro/target/rls/debug/deps/libtokio_macros-48385d003f70045d.so: /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/lib.rs /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/entry.rs /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/select.rs
-
-/home/yjl/study_proj/rust_learn/tokio/select_macro/target/rls/debug/deps/tokio_macros-48385d003f70045d.d: /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/lib.rs /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/entry.rs /home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/select.rs
-
-/home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/lib.rs:
-/home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/entry.rs:
-/home/yjl/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/tokio-macros-0.2.5/src/select.rs:
