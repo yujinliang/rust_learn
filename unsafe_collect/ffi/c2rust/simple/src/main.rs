3e5253e790d53e3f3052bb33e5207827035878ff
yujinliang <285779289@qq.com>
2020-05-23 17:03:17 +0800

    fix

--- unsafe_collect/ffi/c2rust/simple/src/main.rs
+++ unsafe_collect/ffi/c2rust/simple/src/main.rs
@@ -1,4 +1,7 @@
 // src/main.rs
+//reference: https://github.com/Michael-F-Bryan/rust-closures-and-ffi
+//reference:http://adventures.michaelfbryan.com/posts/rust-closures-in-ffi/?utm_source=reddit&utm_medium=social&utm_campaign=rust-closures-in-ffi
+
 mod simple;
 use simple::{ simple_add_two_numbers, two_numbers_added_cb};
 
