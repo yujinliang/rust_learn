1daf4c3eacc12c7f22e541580ed9604d43421cb8
yujinliang <285779289@qq.com>
2020-05-24 11:32:26 +0800

    rust box with c raw pointer

--- unsafe_collect/ffi/c2rust/box_t/src/c_call_rust.c
+++ unsafe_collect/ffi/c2rust/box_t/src/c_call_rust.c
@@ -3,10 +3,14 @@
 // Returns ownership to the caller.
 struct Foo* foo_new(void);
 
+// Returns ownership to the caller.
+struct Foo* foo_new_option(void);
+
 // Takes ownership from the caller; no-op when invoked with NULL.
 void foo_delete(struct Foo*);
 
 int main() {
     foo_delete(foo_new());
      foo_delete(NULL);
+     foo_delete(foo_new_option());
 }
--- unsafe_collect/ffi/c2rust/box_t/src/lib.rs
+++ unsafe_collect/ffi/c2rust/box_t/src/lib.rs
@@ -8,6 +8,11 @@ pub extern "C" fn foo_new() -> Box<Foo> {
     Box::new(Foo)
 }
 
+#[no_mangle]
+pub extern "C" fn foo_new_option() -> Option<Box<Foo>> {
+    Some(Box::new(Foo))
+}
+
 // C`s NULL pointer  is equeal to Option::None.
 #[no_mangle]
 pub extern "C" fn foo_delete(f: Option<Box<Foo>>) {
